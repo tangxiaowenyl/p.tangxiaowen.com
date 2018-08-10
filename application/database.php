4e679efb147138b247b44a1635fc9a06bea8afd4
tangxiaowen.com <846506584@qq.com>
2018-08-10 15:56:23 +0800

    Update database.php

--- application/database.php
+++ application/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '59.110.232.86',
+    'hostname'        => '',
     // 数据库名
     'database'        => '',
     // 用户名
